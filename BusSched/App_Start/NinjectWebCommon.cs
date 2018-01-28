4ea81ec7f16b9758d4923f4096dd411c99618eda
Asher Hardstark <asherhardstark@hotmail.com>
2018-01-28 09:49:06 -0800

    Solution shell

---
+++ BusSched/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using BusSched.App_Start;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+
+namespace BusSched.App_Start
+{
+    public class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        private static void RegisterServices(IKernel kernel)
+        {
+        }
+    }
+}
---
+++ BusSched/Services/AppConfigService.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using BusSched.Services.Services;
+
+namespace BusSched.Services
+{
+    public class AppConfigService : IAppConfigService
+    {
+        public int NumberOfArrivalTimesToReturn
+        {
+            get
+            {
+                int numberOfArrivalTimesToReturn;
+                return int.TryParse(ConfigurationManager.AppSettings["NumberOfArrivalTimesToReturn"], out numberOfArrivalTimesToReturn)
+                    ? numberOfArrivalTimesToReturn
+                    : 1;
+            }
+        }
+    }
+}
